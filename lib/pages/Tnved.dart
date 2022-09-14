8d396294db818385ce41338ea8cb3e3fbd3d23ad
husnidin <31644651+husnidin@users.noreply.github.com>
2022-09-14 07:12:16 +0500

    tnved code and name get

--- lib/pages/Tnved.dart
+++ lib/pages/Tnved.dart
@@ -21,9 +21,9 @@ class _TNVEDState extends State<TNVED> {
 
   var loading = false;
 
-
   GlobalKey<FormState> key = new GlobalKey<FormState>();
   TextEditingController _controller = TextEditingController();
+  String xato = "";
 
   _onSearch(String text) async {
     _serach.clear();
@@ -48,31 +48,56 @@ class _TNVEDState extends State<TNVED> {
     _list.clear();
     try {
       final queryParameters = {"code": _controller.text};
-
       final Uri uri = SimplifiedUri.uri('${apiUrl}', queryParameters);
       final headers = {HttpHeaders.contentTypeHeader: 'application/json'};
       final response = await http.get(uri, headers: headers);
 
-      print("Response status: ${response.statusCode}");
+      print("Response status code: ${response.statusCode}");
       // print("Response body: ${response.body}");
 
       var data = json.decode(response.body);
 
       if (response.statusCode == 200) {
+        setState(() {
+          for (var i in data) {
+            _list.add(TnvedCode.fromJson(i));
+          }
+          loading = false;
+        });
+      } else if (response.statusCode == 404) {
+        final queryParameters = {"name": _controller.text};
+        final Uri uri = SimplifiedUri.uri('${apiUrl}', queryParameters);
+        final headers = {HttpHeaders.contentTypeHeader: 'application/json'};
+        final response = await http.get(uri, headers: headers);
+
+        print("Response status name: ${response.statusCode}");
+        // print("Response body: ${response.body}");
+
+        var data = json.decode(response.body);
+
+        if (response.statusCode == 200) {
           setState(() {
             for (var i in data) {
               _list.add(TnvedCode.fromJson(i));
             }
             loading = false;
           });
-      } else {
-        print("Xatolik");
+        } else if (response.statusCode == 404) {
+          setState(() {
+            xato = _controller.text;
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ErrorModal(xato)),
+            );
+          });
+          // xato="Siz qidiorgan "+_controller.text +" tnved kodi topilmadi";
+          loading = false;
+        }
       }
     } catch (err) {
       print(err);
     }
   }
-
 
   @override
   void initState() {
@@ -136,105 +161,163 @@ class _TNVEDState extends State<TNVED> {
                         ? ListView.builder(
                             itemCount: _list.length,
                             itemBuilder: (context, i) {
-                              final t = _list[i];
-                              return Container(
-                                child: Text(t.id.toString()),
+                              final l = _list[i];
+                              return SingleChildScrollView(
+                                child: Container(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Card(
+                                    color: Colors.white54,
+                                    clipBehavior: Clip.antiAlias,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        ListTile(
+                                          title: Text(
+                                            l.id,
+                                            style: const TextStyle(
+                                                fontSize: 20.0,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          // trailing: Icon(
+                                          //   alreadySaved
+                                          //       ? Icons.star_outlined
+                                          //       : Icons.star_border_outlined,
+                                          //   color: alreadySaved ? Colors.red : null,
+                                          // ),
+                                        ),
+                                        const Divider(
+                                          color: Colors.black12,
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Expanded(
+                                              child: Container(
+                                                padding:
+                                                    const EdgeInsets.all(5.0),
+                                                child: Text(
+                                                  l.name,
+                                                  style: const TextStyle(
+                                                      color: Colors.black,
+                                                      fontWeight:
+                                                          FontWeight.w500),
+                                                ),
+                                              ),
+                                            ),
+                                            Wrap(
+                                              crossAxisAlignment:
+                                                  WrapCrossAlignment.end,
+                                              children: [
+                                                Container(
+                                                  color: Colors.black12,
+                                                  padding:
+                                                      const EdgeInsets.all(5.0),
+                                                  child: Text(
+                                                    l.u1,
+                                                    style: const TextStyle(
+                                                        color: Colors.black),
+                                                  ),
+                                                ),
+                                                Container(
+                                                  color: Colors.black12,
+                                                  padding:
+                                                      const EdgeInsets.all(5.0),
+                                                  child: Text(
+                                                    l.startdate.toString(),
+                                                    style: const TextStyle(
+                                                        color: Colors.black),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
                               );
                             })
                         : ListView.builder(
                             itemCount: _list.length,
                             itemBuilder: (context, i) {
                               final t = _list[i];
-                              return InkWell(
-                                onTap: () {
-                                  // Navigator.push(
-                                  //   context,
-                                  //   MaterialPageRoute(
-                                  //     builder: (context) => Conversiya(
-                                  //         a.code, a.rate, a.ccy, a.ccyNmUz),
-                                  //   ),
-                                  // );
-                                },
+                              return SingleChildScrollView(
                                 child: Container(
-                                  padding: const EdgeInsets.only(
-                                      top: 3, bottom: 3, left: 10, right: 10),
-                                  // color: Colors.white,
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Container(
-                                        margin:
-                                            EdgeInsets.only(bottom: _w / 80),
-                                        height: 75,
-                                        decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius: const BorderRadius.all(
-                                              Radius.circular(20)),
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color:
-                                                  Colors.black.withOpacity(0.3),
-                                              blurRadius: 40,
-                                              spreadRadius: 10,
-                                            ),
-                                          ],
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Card(
+                                    color: Colors.white54,
+                                    clipBehavior: Clip.antiAlias,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        ListTile(
+                                          title: Text(
+                                            t.id,
+                                            style: const TextStyle(
+                                                fontSize: 20.0,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          // trailing: Icon(
+                                          //   alreadySaved
+                                          //       ? Icons.star_outlined
+                                          //       : Icons.star_border_outlined,
+                                          //   color: alreadySaved ? Colors.red : null,
+                                          // ),
                                         ),
-                                        child: Row(
+                                        const Divider(
+                                          color: Colors.black12,
+                                        ),
+                                        Row(
                                           mainAxisAlignment:
                                               MainAxisAlignment.spaceBetween,
                                           children: [
-                                            Wrap(
-                                              children: [
-                                                Container(
-                                                  padding:
-                                                      const EdgeInsets.only(
-                                                          top: 14),
-                                                  child: Text(
-                                                    t.id,
-                                                    style: const TextStyle(
-                                                        fontSize: 20,
-                                                        color:
-                                                            Colors.blueAccent),
-                                                  ),
+                                            Expanded(
+                                              child: Container(
+                                                padding:
+                                                    const EdgeInsets.all(5.0),
+                                                child: Text(
+                                                  t.name,
+                                                  style: const TextStyle(
+                                                      color: Colors.black,
+                                                      fontWeight:
+                                                          FontWeight.w500),
                                                 ),
-                                              ],
+                                              ),
                                             ),
-                                            Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
+                                            Wrap(
                                               crossAxisAlignment:
-                                                  CrossAxisAlignment.end,
+                                                  WrapCrossAlignment.end,
                                               children: [
                                                 Container(
+                                                  color: Colors.black12,
                                                   padding:
-                                                      const EdgeInsets.only(
-                                                          right: 15),
+                                                      const EdgeInsets.all(5.0),
                                                   child: Text(
                                                     t.u1,
                                                     style: const TextStyle(
-                                                        color: Colors.black54,
-                                                        fontSize: 12),
+                                                        color: Colors.black),
                                                   ),
                                                 ),
                                                 Container(
+                                                  color: Colors.black12,
                                                   padding:
-                                                      const EdgeInsets.only(
-                                                          right: 15),
+                                                      const EdgeInsets.all(5.0),
                                                   child: Text(
-                                                    t.unit2,
+                                                    t.startdate.toString(),
                                                     style: const TextStyle(
-                                                        color: Colors.black,
-                                                        fontSize: 20),
+                                                        color: Colors.black),
                                                   ),
                                                 ),
                                               ],
                                             ),
                                           ],
                                         ),
-                                      ),
-                                      // Text(a.ccy),
-                                    ],
+                                      ],
+                                    ),
                                   ),
                                 ),
                               );
@@ -255,6 +338,35 @@ class _TNVEDState extends State<TNVED> {
   Widget _buildRight() {
     return const Center(
       child: Icon(Icons.airplanemode_active),
+    );
+  }
+}
+
+class ErrorModal extends StatelessWidget {
+  final String xato;
+
+  const ErrorModal(String this.xato, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: AlertDialog(
+          title: Text(xato.toString()),
+          titleTextStyle: const TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
+          backgroundColor: Colors.greenAccent,
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20))),
+          content: const Text("Save successfully"),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
